329ca40725c6caf2960d283db54771ba4e7c8183
Bilal <bilalahmed32049.com>
2024-09-02 01:49:40 +0500

    good

--- LMS_project.dart
+++ LMS_project.dart
@@ -1,6 +1,13 @@
 void main() {
   Book book1 = Book("Iqbal Shaheen", "Allama Iqbal", "1234567890", true);
   Book book2 = Book("ISSB", "Pak Army", "1234567891", true);
+  Member member = Member("Bilal", "M101");
+  Libaray libaray = Libaray();
+  libaray.addBook(book1);
+  libaray.addBook(book2);
+  libaray.listBooks();
+  libaray.loanBooks(book2, member);
+  libaray.listloanBooks();
 }
 
 class Book {
@@ -14,7 +21,51 @@ class Book {
 
 class Libaray {
   List<Book> books = [];
+  List<Loan> loans = [];
   void addBook(Book book) {
     books.add(book);
   }
+
+  void listBooks() {
+    print("\n======+ Books in Libarary +======\n");
+    for (var book in books) {
+      print(
+        "Title: ${book.title} \nAuthor: ${book.author}\nISBN: ${book.isbn}\nAvailable: ${book.isAvailable}\n------------------\n",
+      );
+    }
+  }
+
+  void loanBooks(Book book, Member member) {
+    if (book.isAvailable) {
+      book.isAvailable = false;
+      print("${member.name} has loaned ${book.title}.");
+      loans.add(Loan(book, member, DateTime.now()));
+    } else {
+      print("${book.title} is not available.");
+    }
+  }
+
+  void listloanBooks() {
+    print("\n======+ Loan Books of Libarary +======\n");
+    for (var loan in loans) {
+      print(
+        "Title: ${loan.book.title} \nAuthor: ${loan.book.author}\nISBN: ${loan.book.isbn}\nAvailable: ${loan.book.isAvailable}\n------------------\n",
+      );
+    }
+  }
+}
+
+class Member {
+  String name;
+  String id;
+
+  Member(this.name, this.id);
+}
+
+class Loan {
+  Book book;
+  Member member;
+  DateTime loanDate;
+  DateTime? returnDate;
+  Loan(this.book, this.member, this.loanDate, [this.returnDate]);
 }
