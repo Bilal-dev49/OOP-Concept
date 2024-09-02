53f5a74bcfcf4a2a0a6ad17c7a377f397f320a41
Bilal <bilalahmed32049.com>
2024-09-03 02:25:04 +0500

    new

--- inheritance.dart
+++ inheritance.dart
@@ -1,1 +1,7 @@
-
+/* Parent Class (base, super)
+           |
+           |--> Child Class (derived, subclass)
+*/
+// Inheritance in dart is key feature of OOP that allows a class (derived class or subclass)
+// to inherit properties and methods from another class (base class or superclass).
+// This enables code reusibilty and establishes a natural hierarchy between classes
